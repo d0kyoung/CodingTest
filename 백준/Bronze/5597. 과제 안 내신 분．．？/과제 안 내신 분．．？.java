37ca3aa24ebdc0aef45d876b821ce9b138625e17
Dokyoung Lee <72716974+d0kyoung@users.noreply.github.com>
2024-09-26 02:03:57 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int []n = new int[31];
+        for(int i=1;i<29;i++){
+            int temp = Integer.parseInt(br.readLine());
+            n[temp] = 1;
+        }
+        for(int i=1;i<n.length;i++){
+            if(n[i]!=1) System.out.println(i);
+        }
+    }
+}
